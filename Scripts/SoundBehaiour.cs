5ad455b73be7e8b9ca7667066dd79ee693d54044
Faul69 <melvik99@gmail.com>
2023-03-31 15:37:53 +0300

    Added requireComponent

--- Scripts/SoundBehaiour.cs
+++ Scripts/SoundBehaiour.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class SoundBehaiour : MonoBehaviour
 {
     private const int MinVolume = 0;
